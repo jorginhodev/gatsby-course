ff2f971124bf9eb53f029a330c61ced161f863fe
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-12-11 12:51:09 -0300

    Create Profile Component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>Jorge Ramos</h1>
+    <h2>Programador Frontend</h2>
+    <p>
+      Membro do Core Team da Empresa Foo, escrevo sobre o mundo frontend e
+      outras coisas mais.
+    </p>
+  </div>
+)
+
+export default Profile
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,4 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -8,21 +7,6 @@ const Aboutpage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About</h1>
-    <ul>
-      <li>
-        <a href="/about">About (link normal)</a>
-      </li>
-      <li>
-        <Link to="/" activeStyle={{ color: "red" }}>
-          Home
-        </Link>
-      </li>
-      <li>
-        <Link to="/about" activeStyle={{ color: "red" }}>
-          About (Gatsby Link)
-        </Link>
-      </li>
-    </ul>
   </Layout>
 )
 export default Aboutpage
