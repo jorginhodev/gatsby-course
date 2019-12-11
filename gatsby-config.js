5675fafa275dc7bec3f781a591bb7cd10c443d1d
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-12-11 15:02:08 -0300

    Add data with StaticQuery

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `My Blog`,
+    position: `Frontend Developer`,
     description: `A blog about frontend development and other cool stuff.`,
     author: `@jorginhodev`,
   },
