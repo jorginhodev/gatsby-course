4291bd18b544c0ab2ce1e6a25d810e6a6369a2fa
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-12-12 16:10:37 -0300

    Create props to PostItem

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,14 @@ import PostItem from "../components/Postitem"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <PostItem />
+    <PostItem
+      slug="/about/"
+      category="Misc"
+      date="12 de Dezembro de 2019"
+      timeToRead="5"
+      title="Diga não ao Medium: tenha sua própria plataforma"
+      description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+    />
   </Layout>
 )
 
