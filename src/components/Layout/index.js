edce02e5a67f772bce7fd75c365b0962b38fcaa3
Jorge Ramos <jorginhodeveloper@gmail.com>
2019-12-12 10:34:02 -0300

    Refactor styled-components

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,29 +2,18 @@ import React from "react"
 import PropTypes from "prop-types"
 import Profile from "../Profile"
 
-import styled from "styled-components"
+import * as S from "./styled"
 import GlobalStyles from "../../styles/global"
-
-const LayoutWrapper = styled.div`
-  display: flex;
-`
-
-const LayoutMain = styled.div`
-  background: #16202c;
-  min-height: 100vh;
-  padding: 0 3.75rem 0 20rem;
-  width: 100%;
-`
 
 const Layout = ({ children }) => {
   return (
-    <LayoutWrapper>
+    <S.LayoutWrapper>
       <GlobalStyles />
       <aside>
         <Profile />
       </aside>
-      <LayoutMain>{children}</LayoutMain>
-    </LayoutWrapper>
+      <S.LayoutMain>{children}</S.LayoutMain>
+    </S.LayoutWrapper>
   )
 }
 
